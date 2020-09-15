3db4e740c174d636d5781fd526fb998387f72acb
shubham425 <67109872+shubham425@users.noreply.github.com>
2020-09-15 11:00:04 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package Day1Assignments;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hello");
+	}
+
+}
